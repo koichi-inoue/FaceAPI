8ce7b7246a00d6a8b1b693adef58ba0662b603b0
koichi-inoue <inoue.ko@gmail.com>
2021-01-14 16:25:10 +0900

    update script

--- script.js
+++ script.js
@@ -1,56 +1,47 @@
-let faceapi;
+// Preload
+const faceapi = ml5.faceApi(modelLoaded);
 let img;
-let detections;
 let canvas, ctx;
 
-const detection_options = {
-    withLandmarks: true,
-    withDescriptors: false,
-}
+// modelLoaded > Ready to Accept
+function modelLoaded() {
+  document.getElementById('message').innerHTML = '<p>FaceApi model loaded!</p>';
 
-window.onload = function() {
-  var obj = document.getElementById("DropHere");
+  obj = document.getElementById("DropHere");
+  obj.src = 'DropHere.png';
   obj.addEventListener("dragover",function(ev){ ev.preventDefault();}, false);
-  obj.addEventListener("drop", function(ev){ ev.preventDefault(); GetImage(ev);}, false);
+  obj.addEventListener("drop", function(ev){ ev.preventDefault(); Classify(ev);}, false);
 }
 
-function GetImage(ev){
+function Classify(ev){
 
   var file = ev.dataTransfer.files[0];
+
   var fileType = file.name.slice(-4).toLowerCase();
-
-  if( fileType == ".jpg" ){
-
-    var reader = new FileReader();
-    reader.readAsDataURL(file);
-
-    reader.onloadend = function(){
-
-      img = new Image();
-      img.src = reader.result;
-
-      canvas = document.createElement("canvas");
-      canvas.width  = img.naturalWidth;
-      canvas.height = img.naturalHeight;
-
-      document.getElementById("DropHere").remove();
-      document.getElementById("image").appendChild(canvas);
-      ctx = canvas.getContext('2d');
-
-      faceapi = ml5.faceApi(detection_options, modelReady);
-    }
-
-  }else{
-
+  if( fileType !== ".jpg" ) {
     alert("File available only .jpg");
-
+    return;
   }
 
-}
+  var reader = new FileReader();
+  reader.readAsDataURL(file);
 
-function modelReady() {
-    console.log('ready!')
-    faceapi.detectSingle(img, gotResults)
+  reader.onloadend = function() {
+
+    img = new Image();
+    img.src = reader.result;
+
+    canvas = document.createElement("canvas");
+    canvas.width  = img.naturalWidth;
+    canvas.height = img.naturalHeight;
+
+    document.getElementById("DropHere").remove();
+    document.getElementById("image").appendChild(canvas);
+    ctx = canvas.getContext('2d');
+
+    faceapi.detectSingle(img, gotResults);
+  }
+
 }
 
 function gotResults(err, result) {
@@ -58,32 +49,30 @@ function gotResults(err, result) {
         console.log(err)
         return
     }
-    // console.log(result)
-    detections = result;
 
-    if (detections) {
-        console.log(detections)
+    if (result) {
+        console.log(result)
         ctx.drawImage(img, 0,0, img.naturalWidth, img.naturalHeight);
-        drawBox(detections)
-        drawLandmarks(detections)
+        drawBox(result)
+        drawLandmarks(result)
     }
 }
 
-function drawBox(detections){
-    const alignedRect = detections.alignedRect;
+function drawBox(result){
+    const alignedRect = result.alignedRect;
     const {_x, _y, _width, _height} = alignedRect._box;
     ctx.rect(_x, _y, _width, _height);
     ctx.strokeStyle = "#ffffff";
     ctx.stroke();
 }
 
-function drawLandmarks(detections){
+function drawLandmarks(result){
 
     ctx.strokeStyle = "#a15ffb";
 
     // mouth
     ctx.beginPath();
-    detections.parts.mouth.forEach( (item, idx) => {
+    result.parts.mouth.forEach( (item, idx) => {
         if(idx = 0){
             ctx.moveTo(item._x, item._y);
         } else {
@@ -95,7 +84,7 @@ function drawLandmarks(detections){
 
     // nose
     ctx.beginPath();
-    detections.parts.nose.forEach( (item, idx) => {
+    result.parts.nose.forEach( (item, idx) => {
         if(idx = 0){
             ctx.moveTo(item._x, item._y);
         } else {
@@ -106,7 +95,7 @@ function drawLandmarks(detections){
 
     // left eye
     ctx.beginPath();
-    detections.parts.leftEye.forEach( (item, idx) => {
+    result.parts.leftEye.forEach( (item, idx) => {
         if(idx = 0){
             ctx.moveTo(item._x, item._y);
         } else {
@@ -118,7 +107,7 @@ function drawLandmarks(detections){
 
     // right eye
     ctx.beginPath();
-    detections.parts.rightEye.forEach( (item, idx) => {
+    result.parts.rightEye.forEach( (item, idx) => {
         if(idx = 0){
             ctx.moveTo(item._x, item._y);
         } else {
@@ -130,7 +119,7 @@ function drawLandmarks(detections){
 
     // right eyebrow
     ctx.beginPath();
-    detections.parts.rightEyeBrow.forEach( (item, idx) => {
+    result.parts.rightEyeBrow.forEach( (item, idx) => {
         if(idx = 0){
             ctx.moveTo(item._x, item._y);
         } else {
@@ -141,7 +130,7 @@ function drawLandmarks(detections){
 
     // left eyeBrow
     ctx.beginPath();
-    detections.parts.leftEyeBrow.forEach( (item, idx) => {
+    result.parts.leftEyeBrow.forEach( (item, idx) => {
         if(idx = 0){
             ctx.moveTo(item._x, item._y);
         } else {
